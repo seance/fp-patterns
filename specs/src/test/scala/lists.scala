62428ef27ac4f031bf5327bcb046c20a4e908b9c
Jukka Viinamäki <jukka.viinamaki@gmail.com>
2015-03-12 21:52:34 +0200

    Added example solutions for Lists and fixed some bugs

--- specs/src/test/scala/lists.scala
+++ specs/src/test/scala/lists.scala
@@ -5,27 +5,27 @@ class Lists extends Meditation {
   "List basics" in {
     
     "Lists can be constructed using Scala's List constructor" ! {
-      __[List[Int]] must not be empty
+      List(1) must not be empty
     }
     
     "Seq also works but gives static type Seq with List implementation" ! {
-      __[Seq[Int]] must have length(3)
+      Seq(1, 2, 3) must have length(3)
     }
     
     "Bounded ranges can be constructed using to" ! {
-      (1 to 6) must_== __
+      (1 to 6).toList must_== List(1, 2, 3, 4, 5, 6)
     }
     
     "And using until when we want a range with exclusive end" ! {
-      (1 until 6) must_== __
+      (1 until 6).toList must_== List(1, 2, 3, 4, 5)
     }
       
     "List.range works similarly as until" ! {
-      List.range(1, 3) must_== __
+      List.range(1, 3) must_== List(1, 2)
     }
     
     "Bounded ranges can also vary by different steps" ! {
-      (1.0 to 3.0 by 0.5) must_== __
+      (1.0 to 3.0 by 0.5).toList must_== List(1.0, 1.5, 2.0, 2.5, 3.0)
     }
   }
   
@@ -35,38 +35,38 @@ class Lists extends Meditation {
     val xs2: List[Int] = List()
     
     "Head is the first element of a list" ! {
-      xs1.head must_== __
+      xs1.head must_== 1
     }
     
     "Tail is the list without head" ! {
-      xs1.tail must_== __
+      xs1.tail must_== List(2, 3)
     }
     
     "Appending to an immutable list via :+ produces a new list" ! {
-      (xs1 :+ 4) must_== __
+      (xs1 :+ 4) must_== List(1, 2, 3, 4)
     }
     
     "Prepending can be done using +: which also produces a new list" ! {
-      (0 +: xs1) must_== __
+      (0 +: xs1) must_== List(0, 1, 2, 3)
     }
     
     "We can query if a list contains an element via contains" ! {
-      (xs1.contains(2) must_== __) and
-      (xs1.contains(4) must_== __) and
-      (xs2.contains(1) must_== __)
+      (xs1.contains(2) must_== true) and
+      (xs1.contains(4) must_== false) and
+      (xs2.contains(1) must_== false)
     }
     
     "Reduce always produces a value of same type as the list elements" ! {
-      xs1.reduce(_+_) must_== __
+      xs1.reduce(_+_) must_== 6
     }
     
     "Reduce can only be used on non-empty lists" ! {
-      xs2.reduce(_*_) must throwA[__]
+      xs2.reduce(_*_) must throwA[Exception]
     }
     
     "Folding can change the result type and works on empty lists as well" ! {
-      (xs1.foldLeft("0") { case (acc, x) => acc + x.toString } must_== __) and
-      (xs2.foldLeft(List[String]()) { case (acc, x) => List("Hi!") } must_== __)
+      (xs1.foldLeft("0") { case (acc, x) => acc + x.toString } must_== "0123") and
+      (xs2.foldLeft(List[String]()) { case (acc, x) => List("Hi!") } must_== List())
     }
   }
   
@@ -77,11 +77,11 @@ class Lists extends Meditation {
     def nameLength(name: String) = name.length
     
     "Mapping over a list transforms it element-wise" ! {
-      names.map(name => nickNames(name)) must_== __
+      names.map(name => nickNames(name)) must_== List("Ben", "Sam")
     }
     
     "We can chain compositions as before" ! {
-      names.map(nickNames).map(nameLength) must_== __
+      names.map(nickNames).map(nameLength) must_== List(3, 3)
     }
   }
   
@@ -162,7 +162,7 @@ class Lists extends Meditation {
   "Composition using flatMap" in {
       
     "Constructing a plain sequence with flatMap" ! {
-      List(1, 2, 3).__ must_== List(1, 1, 2, 1, 2, 3)
+      List(1, 2, 3).flatMap(n => List.range(1, n+1)) must_== List(1, 1, 2, 1, 2, 3)
     }
     
     import Chess._
@@ -172,7 +172,8 @@ class Lists extends Meditation {
      * from the start position. Use knightStep and flatMap for the
      * implementation.
      */
-    def canKnightReachIn2(start: (Int, Int), end: (Int, Int)): Boolean = __
+    def canKnightReachIn2(start: (Int, Int), end: (Int, Int)): Boolean =
+      knightStep(start).flatMap(knightStep).contains(end)
     
     "The White King's Knight can reach d4 in two moves" ! {
       canKnightReachIn2("g1", "d4") must beTrue
@@ -192,7 +193,11 @@ class Lists extends Meditation {
      * from the start position. Use knightStep and for yield for the
      * implementation.
      */
-    def canKnightReachIn3(start: (Int, Int), end: (Int, Int)): Boolean = __
+    def canKnightReachIn3(start: (Int, Int), end: (Int, Int)): Boolean = (for {
+      step1 <- knightStep(start)
+      step2 <- knightStep(step1)
+      step3 <- knightStep(step2)
+    } yield step3) contains end
     
     "The White Queen's Knight can reach e3 in three moves" ! {
       canKnightReachIn3("b1", "e3") must beTrue
@@ -210,7 +215,7 @@ class Lists extends Meditation {
     yield x3
     
     "Nondeterministically, the empty list represents a dead end" ! {
-      banana must_== __
+      banana must_== List()
     }
   }
   
@@ -225,16 +230,16 @@ class Lists extends Meditation {
     val xs4: List3[Int] = List()
     
     "Nested lists can be un-nested using flatten" ! {
-      xs1.flatten must_== __
+      xs1.flatten must_== List(1, 2, 3)
     }
     
     "Flattening un-nests specifically only one layer" ! {
-      xs2.flatten must_== __
+      xs2.flatten must_== List(List(1), List(2))
     }
     
     "Flattening respects flatMap semantics" ! {
-      (xs3.flatten must_== __) and
-      (xs4.flatten must_== __)
+      (xs3.flatten must_== List()) and
+      (xs4.flatten must_== List())
     }
   }
 }
